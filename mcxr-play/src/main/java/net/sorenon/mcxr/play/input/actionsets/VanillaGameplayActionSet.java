bdcdc76497ed14343939280eee7a857385f10392
Maloonkey <31419110+Maloonkey@users.noreply.github.com>
2022-03-05 12:53:35 -0600

    Fix generic Microsoft WMR bindings
    Currently the inventory and jump button are set to the same button on the right WMR controller, making jumping impossible because you open your inventory at the same time.

--- mcxr-play/src/main/java/net/sorenon/mcxr/play/input/actionsets/VanillaGameplayActionSet.java
+++ mcxr-play/src/main/java/net/sorenon/mcxr/play/input/actionsets/VanillaGameplayActionSet.java
@@ -106,12 +106,12 @@ public class VanillaGameplayActionSet extends ActionSet {
                         new Pair<>(move, "/user/hand/left/input/thumbstick"),
                         new Pair<>(hotbar, "/user/hand/right/input/thumbstick/y"),
                         new Pair<>(turn, "/user/hand/right/input/thumbstick/x"),
-                        new Pair<>(inventory, "/user/hand/left/input/trackpad/click"),
+                        new Pair<>(inventory, "/user/hand/left/input/menu/click"),
                         new Pair<>(jump, "/user/hand/right/input/trackpad/click"),
                         new Pair<>(sprint, "/user/hand/right/input/squeeze/click"),
                         new Pair<>(sneak, "/user/hand/left/input/squeeze/click"),
-                        new Pair<>(resetPos, "/user/hand/right/input/thumbstick/click"),
-                        new Pair<>(inventory, "/user/hand/right/input/trackpad/click")
+                        new Pair<>(resetPos, "/user/hand/left/input/trackpad/click"),
+                        new Pair<>(inventory, "/user/hand/right/input/menu/click")
                 )
         );
         if (MCXRPlayClient.OPEN_XR.instance.handle.getCapabilities().XR_EXT_hp_mixed_reality_controller) {

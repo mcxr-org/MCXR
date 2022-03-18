e9888a43415661e8df063e08a16b690c693df1a3
Sorenon <sorenonandstuffs@gmail.com>
2022-03-18 22:06:56 +0000

    trident throw animation

--- mcxr-play/src/main/java/net/sorenon/mcxr/play/rendering/VrFirstPersonRenderer.java
+++ mcxr-play/src/main/java/net/sorenon/mcxr/play/rendering/VrFirstPersonRenderer.java
@@ -388,21 +388,15 @@ public class VrFirstPersonRenderer {
                     }
 
                     if (stack.getItem() == Items.TRIDENT && player.getUseItem() == stack) {
-                        float k = (float)stack.getUseDuration() - ((float)player.getUseItemRemainingTicks() - deltaTick + 1.0F);
-
-                        float l = k / 10.0F;
-                        if (l > 1.0F) {
-                            l = 1.0F;
-                        }
-
+                        float k = (float) stack.getUseDuration() - ((float) player.getUseItemRemainingTicks() - deltaTick + 1);
+                        float l = Math.min(k / 10, 1);
                         if (l > 0.1F) {
-                            float m = Mth.sin((k - 0.1F) * 1.3F);
-                            float n = l - 0.1F;
+                            float m = Mth.sin((k - 0.1f) * 1.3f);
+                            float n = l - 0.1f;
                             float o = m * n;
-                            matrices.translate(0, o * 0.004F, 0);
+                            matrices.translate(0, o * 0.004, 0);
                         }
                         matrices.translate(0, 0, l * 0.2);
-
                         matrices.mulPose(Quaternion.fromXYZ(Math.toRadians(90), 0, 0));
                     }
 

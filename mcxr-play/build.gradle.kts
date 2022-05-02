d846f6929deb72f50bbbdc1279f0dcb8e1d535ee
The Judge <colepatrickroberts@gmail.com>
2022-05-01 20:47:09 -0700

    Fix gradle problems

--- mcxr-play/build.gradle.kts
+++ mcxr-play/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-    id("fabric-loom") version "0.10-SNAPSHOT"
-    id("io.github.juuxel.loom-quiltflower-mini") version "1.2.1"
+    id("fabric-loom") version "0.11-SNAPSHOT"
+    id("io.github.juuxel.loom-quiltflower") version "1.7.1"
     id("maven-publish")
-    id("org.quiltmc.quilt-mappings-on-loom") version "3.1.1"
+    id("org.quiltmc.quilt-mappings-on-loom") version "4.2.0"
     id("org.ajoberstar.grgit") version "4.1.0"
 }
 

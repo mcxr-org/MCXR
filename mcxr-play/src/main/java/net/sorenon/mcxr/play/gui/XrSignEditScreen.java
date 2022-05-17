d288a58bfe528a0a9f1b57aee425076d252788be
Israel Aristide <44146685+IsraelAristide@users.noreply.github.com>
2022-05-16 22:05:08 -0400

    Vr Keyboard code refactor.
    Ok this is the last time i change all this i promise :P
    Code is much easier to understand now with the VR keyboard and is much easier to customize and implement in other places.

---
+++ mcxr-play/src/main/java/net/sorenon/mcxr/play/gui/XrSignEditScreen.java
@@ -0,0 +1,69 @@
+package net.sorenon.mcxr.play.gui;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.components.AbstractWidget;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.world.level.block.entity.SignBlockEntity;
+import net.sorenon.mcxr.play.gui.keyboard.XrSignKeyboard;
+
+public class XrSignEditScreen extends Screen {
+
+    private final SignBlockEntity _sign;
+    private final XrSignKeyboard _keyboard;
+
+    public XrSignEditScreen(TranslatableComponent title, SignBlockEntity sign) {
+
+        super(title);
+        _sign = sign;
+
+        this.width = Minecraft.getInstance().getWindow().getWidth();
+        this.height = Minecraft.getInstance().getWindow().getHeight();
+
+        _keyboard = new XrSignKeyboard(this);
+
+    }
+
+    public void clear() {
+        this.clearWidgets();
+    }
+
+    public SignBlockEntity getSign() {
+        return _sign;
+    }
+
+    public void addRenderWidget(AbstractWidget widget) {
+        this.addRenderableWidget(widget);
+    }
+
+    @Override
+    protected void init() {
+
+        _keyboard.renderKeyboard(_keyboard.getDefaultCharset(), this.width, this.height, 30);
+        super.init();
+
+    }
+
+    @Override
+    public void render(PoseStack matrices, int mouseX, int mouseY, float delta) {
+        super.render(matrices, mouseX, mouseY, delta);
+
+        if (_keyboard.getTextField1().isFocused()) {
+            _keyboard.setActiveTextField(_keyboard.getTextField1());
+        }
+
+        if (_keyboard.getTextField2().isFocused()) {
+            _keyboard.setActiveTextField(_keyboard.getTextField2());
+        }
+
+        if (_keyboard.getTextField3().isFocused()) {
+            _keyboard.setActiveTextField(_keyboard.getTextField3());
+        }
+
+        if (_keyboard.getTextField4().isFocused()) {
+            _keyboard.setActiveTextField(_keyboard.getTextField4());
+        }
+
+    }
+}

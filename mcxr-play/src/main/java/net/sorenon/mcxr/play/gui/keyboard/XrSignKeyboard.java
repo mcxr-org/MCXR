4cbcf79a333d0cc299f3924050b2801f82a97246
Israel Aristide <44146685+IzzyDotExe@users.noreply.github.com>
2022-05-17 14:13:10 -0400

    Keyboard improvements and bump Pehkui. (#62)
    * - Added Shift and capslock modifiers to the keyboard
    - pehkui version bump
    
    * Added Missing Mixin for adding servers within VR
    
    * Removed questcraft logger
    
    * Only use the VR keyboard when in VR mode.
    
    * Vr Keyboard code refactor.
    
    Ok this is the last time i change all this i promise :P
    Code is much easier to understand now with the VR keyboard and is much easier to customize and implement in other places.
    
    * Vr Keyboard code refactor.
    
    Ok this is the last time i change all this i promise :P
    Code is much easier to understand now with the VR keyboard and is much easier to customize and implement in other places.
    
    there is also now support for editing signs
    
    Co-authored-by: Israel Aristide <44146685+IsraelAristide@users.noreply.github.com>

---
+++ mcxr-play/src/main/java/net/sorenon/mcxr/play/gui/keyboard/XrSignKeyboard.java
@@ -0,0 +1,117 @@
+package net.sorenon.mcxr.play.gui.keyboard;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.components.EditBox;
+import net.minecraft.network.chat.TranslatableComponent;
+import net.sorenon.mcxr.play.gui.XrSignEditScreen;
+
+public class XrSignKeyboard extends XrAbstract2DKeyboard {
+
+    private final EditBox _textField1;
+    private final EditBox _textField2;
+    private final EditBox _textField3;
+    private final EditBox _textField4;
+
+    private EditBox _activeTextField;
+
+    private final XrSignEditScreen _signEditScreen;
+
+    private boolean _shift, _caps;
+
+    public XrSignKeyboard(XrSignEditScreen signEditScreen) {
+
+        _signEditScreen = signEditScreen;
+        _textField1 = new EditBox(Minecraft.getInstance().font,
+                (_signEditScreen.width /3) - 160, 0, 160, 10, new TranslatableComponent(""));
+        _textField2 = new EditBox(Minecraft.getInstance().font,
+                (_signEditScreen.width /3) - 160, 10, 160, 10, new TranslatableComponent(""));
+        _textField3 = new EditBox(Minecraft.getInstance().font,
+                (_signEditScreen.width /3) - 160, 20, 160, 10, new TranslatableComponent(""));
+        _textField4 = new EditBox(Minecraft.getInstance().font,
+                (_signEditScreen.width /3) - 160, 30, 160, 10, new TranslatableComponent(""));
+
+        _activeTextField = _textField1;
+    }
+
+    @Override
+    public void returnButton(Button instance) {
+        _signEditScreen.getSign().setMessage(0, new TranslatableComponent(_textField1.getValue()));
+        _signEditScreen.getSign().setMessage(1, new TranslatableComponent(_textField2.getValue()));
+        _signEditScreen.getSign().setMessage(2, new TranslatableComponent(_textField3.getValue()));
+        _signEditScreen.getSign().setMessage(3, new TranslatableComponent(_textField4.getValue()));
+        _signEditScreen.onClose();
+    }
+
+    public EditBox getTextField1() {
+        return _textField1;
+    }
+
+    public EditBox getTextField2() {
+        return _textField2;
+    }
+
+    public EditBox getTextField3() {
+        return _textField3;
+    }
+
+    public EditBox getTextField4() {
+        return _textField4;
+    }
+
+    public EditBox getActiveTextField() {
+        return _activeTextField;
+    }
+
+    public void setActiveTextField(EditBox _activeTextField) {
+        this._activeTextField = _activeTextField;
+    }
+
+    @Override
+    public void backSpaceButton(Button instance) {
+        _activeTextField.setValue(removeLastChar(_activeTextField.getValue()));
+    }
+
+    @Override
+    public void spaceButton(Button button) {
+        _activeTextField.setValue(_activeTextField.getValue() + " ");
+    }
+
+    @Override
+    public void tabButton(Button button) {
+        _activeTextField.setValue(_activeTextField.getValue() + "    ");
+    }
+
+    @Override
+    public void shiftButton(Button button) {
+        _shift = !_shift;
+        this._signEditScreen.clear();
+        renderKeyboard(_shift? this.getShiftCharset() : _caps? this.getCapsCharset() : this.getDefaultCharset(), _signEditScreen.width, _signEditScreen.height, 30);
+    }
+
+    @Override
+    public void capsButton(Button button) {
+        _caps = !_caps;
+        this._signEditScreen.clear();
+        renderKeyboard(_shift? this.getShiftCharset() : _caps? this.getCapsCharset() : this.getDefaultCharset(), _signEditScreen.width, _signEditScreen.height, 30);
+    }
+
+    @Override
+    public void letterButton(Button instance) {
+        String stringText = _activeTextField.getValue() + instance.getMessage().getString();
+        _activeTextField.setValue(stringText);
+    }
+
+    @Override
+    public void renderKey(Button key) {
+        _signEditScreen.addRenderWidget(key);
+    }
+
+    @Override
+    public void afterRender() {
+        _signEditScreen.addRenderWidget(_textField1);
+        _signEditScreen.addRenderWidget(_textField2);
+        _signEditScreen.addRenderWidget(_textField3);
+        _signEditScreen.addRenderWidget(_textField4);
+    }
+}
---
+++ mcxr-play/src/main/java/net/sorenon/mcxr/play/mixin/MultiPlayerScreenMixin.java
@@ -0,0 +1,50 @@
+package net.sorenon.mcxr.play.mixin;
+
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.components.events.GuiEventListener;
+import net.minecraft.client.gui.screens.DirectJoinServerScreen;
+import net.minecraft.client.gui.screens.EditServerScreen;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.gui.screens.multiplayer.JoinMultiplayerScreen;
+import net.minecraft.client.multiplayer.ServerData;
+import net.minecraft.client.resources.language.I18n;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TranslatableComponent;
+import net.sorenon.mcxr.play.MCXRPlayClient;
+import net.sorenon.mcxr.play.PlayOptions;
+import net.sorenon.mcxr.play.gui.AddServerScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(JoinMultiplayerScreen.class)
+public abstract class MultiPlayerScreenMixin extends Screen {
+
+
+    @Shadow protected abstract void addServerCallback(boolean confirmedAction);
+
+    @Shadow private ServerData editingServer;
+
+    protected MultiPlayerScreenMixin(Component component) {
+        super(component);
+    }
+
+    @Redirect(method = "init", at=@At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screens/multiplayer/JoinMultiplayerScreen;addRenderableWidget(Lnet/minecraft/client/gui/components/events/GuiEventListener;)Lnet/minecraft/client/gui/components/events/GuiEventListener;", ordinal = 2))
+    public GuiEventListener addrenderableMixin(JoinMultiplayerScreen instance, GuiEventListener guiEventListener) {
+        if (!PlayOptions.xrUninitialized) {
+            if (FabricLoader.getInstance().isModLoaded("titleworlds")) {
+                return addRenderableWidget(new Button(this.width / 2 + 54, this.height - 52, 100, 20, new TranslatableComponent("selectServer.add"), (button) -> {
+                    this.minecraft.setScreen(new AddServerScreen(new TranslatableComponent("Add server"), instance));
+                }));
+            }
+        }
+
+        return addRenderableWidget(new Button(this.width / 2 + 54, this.height - 52, 100, 20, new TranslatableComponent("selectServer.add"), (button) -> {
+            this.editingServer = new ServerData(I18n.get("selectServer.defaultName", new Object[0]), "", false);
+            this.minecraft.setScreen(new EditServerScreen(this, this::addServerCallback, this.editingServer));
+        }));
+
+    }
+}

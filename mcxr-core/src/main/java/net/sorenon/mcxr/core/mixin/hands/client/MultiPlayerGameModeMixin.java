9d2667834b8865ba82561a43948bc2f351bc1278
The Judge <53906078+ManiacGamer156@users.noreply.github.com>
2022-06-05 21:29:25 -0700

    Fix another fatal mixin error

--- mcxr-core/src/main/java/net/sorenon/mcxr/core/mixin/hands/client/MultiPlayerGameModeMixin.java
+++ mcxr-core/src/main/java/net/sorenon/mcxr/core/mixin/hands/client/MultiPlayerGameModeMixin.java
@@ -1,12 +1,14 @@
 package net.sorenon.mcxr.core.mixin.hands.client;
 
 import net.minecraft.client.multiplayer.MultiPlayerGameMode;
+import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.BlockHitResult;
 import net.sorenon.mcxr.core.MCXRCore;
 import net.sorenon.mcxr.core.accessor.PlayerExt;
 import org.spongepowered.asm.mixin.Mixin;
@@ -17,23 +19,17 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(MultiPlayerGameMode.class)
 public class MultiPlayerGameModeMixin {
 
-    @Inject(method = "useItem", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;use(Lnet/minecraft/world/level/Level;Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/world/InteractionHand;)Lnet/minecraft/world/InteractionResultHolder;"))
-    void preUse(Player player,
-                Level level,
-                InteractionHand interactionHand,
-                CallbackInfoReturnable<InteractionResult> cir) {
-        PlayerExt playerExt = ((PlayerExt) player);
+    @Inject(method = "performUseItemOn", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;useOn(Lnet/minecraft/world/item/context/UseOnContext;)Lnet/minecraft/world/InteractionResult;"))
+    void preUse(LocalPlayer localPlayer, InteractionHand interactionHand, BlockHitResult blockHitResult, CallbackInfoReturnable<InteractionResult> cir) {
+        PlayerExt playerExt = ((PlayerExt) localPlayer);
         if (playerExt.isXR()) {
-            playerExt.getOverrideTransform().set(MCXRCore.handToArm(player, interactionHand));
+            playerExt.getOverrideTransform().set(MCXRCore.handToArm(localPlayer, interactionHand));
         }
     }
 
-    @Inject(method = "useItem", at = @At(value = "INVOKE", shift = At.Shift.AFTER, target = "Lnet/minecraft/world/item/ItemStack;use(Lnet/minecraft/world/level/Level;Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/world/InteractionHand;)Lnet/minecraft/world/InteractionResultHolder;"))
-    void postUse(Player player,
-                 Level level,
-                 InteractionHand interactionHand,
-                 CallbackInfoReturnable<InteractionResult> cir) {
-        PlayerExt playerExt = ((PlayerExt) player);
+    @Inject(method = "performUseItemOn", at = @At(value = "INVOKE", shift = At.Shift.AFTER, target = "Lnet/minecraft/world/item/ItemStack;useOn(Lnet/minecraft/world/item/context/UseOnContext;)Lnet/minecraft/world/InteractionResult;"))
+    void postUse(LocalPlayer localPlayer, InteractionHand interactionHand, BlockHitResult blockHitResult, CallbackInfoReturnable<InteractionResult> cir) {
+        PlayerExt playerExt = ((PlayerExt) localPlayer);
         if (playerExt.isXR()) {
             playerExt.getOverrideTransform().set(null);
         }

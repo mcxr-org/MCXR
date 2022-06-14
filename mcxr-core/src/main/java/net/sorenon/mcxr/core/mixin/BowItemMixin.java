a0ad9b1a7fea587346550f2bd0139a2bbea0486d
The Judge <53906078+thejudge156@users.noreply.github.com>
2022-06-13 21:18:47 -0700

    Remove crossbow for now, fix fatal mixin error

--- mcxr-core/src/main/java/net/sorenon/mcxr/core/mixin/BowItemMixin.java
+++ mcxr-core/src/main/java/net/sorenon/mcxr/core/mixin/BowItemMixin.java
@@ -17,7 +17,7 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 @Mixin(BowItem.class)
 public class BowItemMixin {
     @Redirect(method = "releaseUsing", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/projectile/AbstractArrow;shootFromRotation(Lnet/minecraft/world/entity/Entity;FFFFF)V"))
-    private static void shootFromArm(AbstractArrow instance, Entity entity, float playerXRot, float playerYRot, float roll, float modifierZ, float modifierXYZ) {
+    private void shootFromArm(AbstractArrow instance, Entity entity, float playerXRot, float playerYRot, float roll, float modifierZ, float modifierXYZ) {
         if(entity instanceof Player player) {
             PlayerExt acc = (PlayerExt) player;
             Pose arm = acc.getPoseForArm(player.getMainArm());

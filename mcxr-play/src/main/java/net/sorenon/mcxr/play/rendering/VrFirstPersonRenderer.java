98bd03cd4c1e55843923e73145e4b56f1df940b4
Xan <70198941+xanthousm@users.noreply.github.com>
2022-06-25 12:19:13 +1000

    Attack/swing animation tweaks

--- mcxr-play/src/main/java/net/sorenon/mcxr/play/rendering/VrFirstPersonRenderer.java
+++ mcxr-play/src/main/java/net/sorenon/mcxr/play/rendering/VrFirstPersonRenderer.java
@@ -419,6 +419,11 @@ public class VrFirstPersonRenderer {
 
         matrices.translate(0, 1 / 16f, -1.5f / 16f);
         matrices.mulPose(com.mojang.math.Vector3f.XP.rotationDegrees(PlayOptions.handPitchAdjust));
+
+        if (hand == MCXRPlayClient.getMainHand()) {
+            float swing = -0.1f * Mth.sin((float) (Math.sqrt(Minecraft.getInstance().player.getAttackAnim(tickDelta)) * Math.PI * 2));
+            matrices.translate(0,0, swing);
+        }
     }
 
     public void renderShadow(WorldRenderContext context, Entity camEntity) {
@@ -495,7 +500,7 @@ public class VrFirstPersonRenderer {
                     transformToHand(matrices, handIndex, deltaTick);
 
                     if (handIndex == MCXRPlayClient.getMainHand()) {
-                        float swing = -0.4f * Mth.sin((float) (Math.sqrt(player.getAttackAnim(deltaTick)) * Math.PI * 2));
+                        float swing = -0.6f * Mth.sin((float) (Math.sqrt(player.getAttackAnim(deltaTick)) * Math.PI * 2));
                         matrices.mulPose(com.mojang.math.Vector3f.XP.rotation(swing));
                     }
 

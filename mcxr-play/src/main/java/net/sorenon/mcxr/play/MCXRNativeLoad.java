5729802adf30e0ec575c0d0b4dba7c72d6590435
CADIndie <84008186+CADIndie@users.noreply.github.com>
2022-08-09 16:55:40 -0400

    1.18.2 Backport | Stuff broky...

---
+++ mcxr-play/src/main/java/net/sorenon/mcxr/play/MCXRNativeLoad.java
@@ -0,0 +1,12 @@
+
+package net.sorenon.mcxr.play;
+
+public class MCXRNativeLoad {
+    static {
+        System.loadLibrary("mcxr_loader");
+    }
+
+    public static native long getJVMPtr();
+    public static native long getApplicationActivityPtr();
+    public static native void renderImage(int colorAttachment, int index);
+}
